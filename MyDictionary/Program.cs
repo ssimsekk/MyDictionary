5f353fa766dbcf165b4edfac7c4df9a8225183de
ssimsekk <suleymansimsekk@gmail.com>
2021-01-23 19:17:41 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> kelimeler = new MyDictionary<string>();
+            kelimeler.Add("Justice");
+
+        }
+    }
+}
